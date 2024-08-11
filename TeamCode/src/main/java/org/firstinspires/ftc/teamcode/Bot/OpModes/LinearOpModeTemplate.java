c82d020540e85bae68913539e0d6f7058e1f59d9
Emma <emmaperezrovira@gmail.com>
2024-08-11 11:28:52 -0400

    Added the barebones to the LinearOpModeTemplate. Now has a wait for start and a while active loop.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/OpModes/LinearOpModeTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/OpModes/LinearOpModeTemplate.java
@@ -1,4 +1,15 @@
 package org.firstinspires.ftc.teamcode.Bot.OpModes;
 
-public class LinearOpModeTemplate {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class LinearOpModeTemplate extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        waitForStart();
+        while(opModeIsActive()){
+
+        }
+    }
 }
