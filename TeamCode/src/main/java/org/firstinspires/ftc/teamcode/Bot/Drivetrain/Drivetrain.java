70d9ca8106f5f3a36324b449b2788b5f743eeb42
maggie zhu <107164272+magizee@users.noreply.github.com>
2024-09-12 16:47:14 -0400

    creating drivetrain from scratch -- added initializtion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/Drivetrain/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/Drivetrain/Drivetrain.java
@@ -1,15 +1,88 @@
 package org.firstinspires.ftc.teamcode.Bot.Drivetrain;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.Bot.Setup;
+import org.firstinspires.ftc.teamcode.PedroPathing.follower.Follower;
+import org.firstinspires.ftc.teamcode.PedroPathing.localization.ThreeWheelLocalizer;
+import org.firstinspires.ftc.teamcode.PedroPathing.pathGeneration.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Drivetrain {
-    public void init(){
+    public Pose2d currentPos;
 
+    protected DcMotorEx leftFront;
+    protected DcMotorEx rightFront;
+    protected DcMotorEx leftRear;
+    protected DcMotorEx rightRear;
+
+    private Vector driveVector;
+    private Vector headingVector;
+    private Follower follower;
+
+    private IMU imu;
+    private VoltageSensor batteryVoltageSensor;
+    private boolean useIMU;
+
+    private List<Integer> lastEncPositions = new ArrayList<>();
+    private List<Integer> lastEncVels = new ArrayList<>();
+
+
+    public Drivetrain(HardwareMap hardwareMap){
+        follower = new Follower(hardwareMap, false);
+
+        driveVector = new Vector();
+        headingVector = new Vector();
+
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
+        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        rightRear.setDirection(DcMotorEx.Direction.REVERSE); // rightRear, rightFront reverse for comp bot
+        rightFront.setDirection(DcMotorEx.Direction.REVERSE); // rightRear, leftRear reverse for test bot
     }
 
+    public void init(Pose2d startPose) {
+        currentPos = startPose;
+    }
     public void update(){
-
+    }
+    public void telemetry(){
+        Setup.telemetry.addData("Drivetrain currentPos", currentPos);
     }
 
-    public void telemetry(){
+    public double getHeadingIMU() {
+        return 0;
+    }
+    public void setCurrentPose(Pose2d pose) {
+        currentPos = pose;
+    }
+    public Pose2d getCurrentPose() {
+        return currentPos;
+    }
 
+    public void setMotorPowers(double vLeftFront, double vLeftRear, double vRightRear, double vRightFront) {
+        leftFront.setPower(vLeftFront);
+        leftRear.setPower(vLeftRear);
+        rightRear.setPower(vRightRear);
+        rightFront.setPower(vRightFront);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/OpModes/LinearOpModeTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/OpModes/LinearOpModeTemplate.java
@@ -1,86 +1,86 @@
-package org.firstinspires.ftc.teamcode.Bot.OpModes;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.Bot.Bot;
-import org.firstinspires.ftc.teamcode.Bot.Setup;
-
-public class LinearOpModeTemplate extends LinearOpMode {
-    Bot bot;
-    Setup setup;
-    ElapsedTime timer = new ElapsedTime();
-
-    @Override
-    public void runOpMode()throws InterruptedException {
-        setup = new Setup(hardwareMap, telemetry, true, this, Setup.OpModeType.TELEOP, Setup.Team.Q1);
-        setup.disableMechanism("outtakeSlides");
-
-        bot = new Bot(setup.mechStates, setup.sensorStates);
-        bot.init();
-
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        waitForStart();
-        timer.reset();
-
-        while (opModeIsActive()){
-            driver1();
-            driver2();
-            manualDriver2();
-
-            bot.update();
-            bot.telemetry();
-        }
-
-    }
-    double x, y, spin, angle, translateMag;
-    double imuOffset = Math.toRadians(0);
-    void driver1(){
-        if (gamepad1.left_trigger > 0.1){
-            imuOffset = bot.drivetrain.getHeadingIMU();
-        }
-
-        x = Math.abs(gamepad1.left_stick_x)>0.04 ? gamepad1.left_stick_x : 0;
-        y = Math.abs(gamepad1.left_stick_y)>0.04 ? -gamepad1.left_stick_y : 0;
-        spin = Math.abs(gamepad1.right_stick_x) > 0.04 ? gamepad1.right_stick_x : 0;
-        translateMag = Math.sqrt(x*x + y*y);
-        angle = Math.atan2(y, x);
-
-        angle += (-bot.drivetrain.getHeadingIMU() + imuOffset);
-
-
-        x = Math.cos(angle) * translateMag;
-        y = Math.sin(angle) * translateMag;
-
-        if (gamepad1.left_bumper) {
-            x *= 0.25;
-            y *= 0.25;
-            spin *= 0.25;
-        } else if(bot.outtakeSlides.getCurrentPosition()>100){
-            x *= 0.7;
-            y *= 0.7;
-            spin *= 0.4;
-        }
-//        else if (gamepad1.right_bumper) {
-//            x *= 1;
-//            y *= 1;
-//            spin *= 1;
+//package org.firstinspires.ftc.teamcode.Bot.OpModes;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.util.ElapsedTime;
+//
+//import org.firstinspires.ftc.teamcode.Bot.Bot;
+//import org.firstinspires.ftc.teamcode.Bot.Setup;
+//
+//public class LinearOpModeTemplate extends LinearOpMode {
+//    Bot bot;
+//    Setup setup;
+//    ElapsedTime timer = new ElapsedTime();
+//
+//    @Override
+//    public void runOpMode()throws InterruptedException {
+//        setup = new Setup(hardwareMap, telemetry, true, this, Setup.OpModeType.TELEOP, Setup.Team.Q1);
+//        setup.disableMechanism("outtakeSlides");
+//
+//        bot = new Bot(setup.mechStates, setup.sensorStates);
+//        bot.init();
+//
+//        telemetry.addData("Status", "Initialized");
+//        telemetry.update();
+//
+//        waitForStart();
+//        timer.reset();
+//
+//        while (opModeIsActive()){
+//            driver1();
+//            driver2();
+//            manualDriver2();
+//
+//            bot.update();
+//            bot.telemetry();
 //        }
-        else {
-            x *= .95;
-            y *= 1;
-            spin *= .7;
-        }
-
-        bot.drivetrain.setPowerTargets(new double[]{x, y, spin}, false, false);
-        telemetry.addData("translateMag", translateMag);
-        telemetry.addData("x", x);
-        telemetry.addData("y", y);
-        telemetry.addData("spin", spin);
-        telemetry.addData("angle", angle);
-
-    }
-    }
-}
+//
+//    }
+//    double x, y, spin, angle, translateMag;
+//    double imuOffset = Math.toRadians(0);
+//    void driver1(){
+//        if (gamepad1.left_trigger > 0.1){
+//            imuOffset = bot.drivetrain.getHeadingIMU();
+//        }
+//
+//        x = Math.abs(gamepad1.left_stick_x)>0.04 ? gamepad1.left_stick_x : 0;
+//        y = Math.abs(gamepad1.left_stick_y)>0.04 ? -gamepad1.left_stick_y : 0;
+//        spin = Math.abs(gamepad1.right_stick_x) > 0.04 ? gamepad1.right_stick_x : 0;
+//        translateMag = Math.sqrt(x*x + y*y);
+//        angle = Math.atan2(y, x);
+//
+//        angle += (-bot.drivetrain.getHeadingIMU() + imuOffset);
+//
+//
+//        x = Math.cos(angle) * translateMag;
+//        y = Math.sin(angle) * translateMag;
+//
+//        if (gamepad1.left_bumper) {
+//            x *= 0.25;
+//            y *= 0.25;
+//            spin *= 0.25;
+//        } else if(bot.outtakeSlides.getCurrentPosition()>100){
+//            x *= 0.7;
+//            y *= 0.7;
+//            spin *= 0.4;
+//        }
+////        else if (gamepad1.right_bumper) {
+////            x *= 1;
+////            y *= 1;
+////            spin *= 1;
+////        }
+//        else {
+//            x *= .95;
+//            y *= 1;
+//            spin *= .7;
+//        }
+//
+//        bot.drivetrain.setPowerTargets(new double[]{x, y, spin}, false, false);
+//        telemetry.addData("translateMag", translateMag);
+//        telemetry.addData("x", x);
+//        telemetry.addData("y", y);
+//        telemetry.addData("spin", spin);
+//        telemetry.addData("angle", angle);
+//
+//    }
+//    }
+//}
