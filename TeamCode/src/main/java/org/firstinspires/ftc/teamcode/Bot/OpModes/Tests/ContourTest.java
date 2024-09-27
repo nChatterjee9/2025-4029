01d239159d6931a29310502ae74a280834dc8441
imsmelkov <imsmelkov@gmail.com>
2024-09-27 13:01:09 -0400

    Completed pipeline for locating colored game pieces. Implemented this in new file, ContourTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/OpModes/Tests/ContourTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.Bot.OpModes.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Bot.Sensors.Vision.Pipelines.Contour;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+public class ContourTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int cameraMoniterViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        OpenCvWebcam webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "webcam"), cameraMoniterViewId);
+        webcam.setMillisecondsPermissionTimeout(2500);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+        webcam.setPipeline(new Contour());
+        waitForStart();
+        webcam.closeCameraDeviceAsync(() -> {
+            webcam.stopStreaming();
+        });
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/Sensors/Vision/Pipelines/Contour.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/Sensors/Vision/Pipelines/Contour.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.Bot.Sensors.Vision.Pipelines;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
@@ -46,7 +48,29 @@ public class Contour extends OpenCvPipeline {
         } else {
             Imgproc.findContours(red, contours, dummy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
         }
-        return null;
+        int currentArrayPos = 0;
+        int biggestBox = 0;
+        int biggestBoxSize = -1;
+        Rect boundingRect;
+        for(MatOfPoint contour : contours){
+            boundingRect = Imgproc.boundingRect(contour);
+
+            if(boundingRect.height * boundingRect.width > biggestBoxSize){
+                biggestBox = currentArrayPos;
+                biggestBoxSize = boundingRect.height * boundingRect.width;
+            }
+
+            currentArrayPos++;
+        }
+
+        boundingRect = Imgproc.boundingRect(contours.get(biggestBox));
+        Imgproc.rectangle(HSV, boundingRect.tl(), boundingRect.br(), new Scalar(0, 0, 255), 2);
+        Imgproc.circle(HSV, new Point(boundingRect.tl().x + boundingRect.width/2, boundingRect.tl().y + boundingRect.height/2), 10, new Scalar(0,0,255), 2);
+
+        Imgproc.cvtColor(HSV, input, Imgproc.COLOR_HSV2RGB);
+
+
+        return input;
     }
 
 }
