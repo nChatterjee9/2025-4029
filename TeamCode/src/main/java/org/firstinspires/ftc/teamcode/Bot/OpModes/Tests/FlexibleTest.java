796caa16ac3a21848f6a5f5bf2f83e57111b2595
maggie zhu <107164272+magizee@users.noreply.github.com>
2024-09-27 13:41:17 -0400

    flexible test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/OpModes/Tests/FlexibleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/OpModes/Tests/FlexibleTest.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.Bot.OpModes.Tests;
 
-public class FlexibleTest {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Bot.Mechanisms.AbstractMechanisms.Mechanism;
+import org.firstinspires.ftc.teamcode.Bot.Mechanisms.MotorExample;
+import org.firstinspires.ftc.teamcode.Bot.Mechanisms.ServoExample;
+import org.firstinspires.ftc.teamcode.Bot.Setup;
+
+import java.util.Set;
+
+public class FlexibleTest extends LinearOpMode {
+    Setup setup;
+
+    Mechanism mechanism;
+    Mechanism motor = new MotorExample(), servo = new ServoExample();
+
+    double targetPos, futureVelPos, changeInPos, velocity;
+    double SERVO_INCREMENT = 0.001, MOTOR_INCREMENT = 5;
+    ElapsedTime timer = new ElapsedTime();
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 }
