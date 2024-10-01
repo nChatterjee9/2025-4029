17356dbcf4206cae60a75792cd795394b4e8b61e
maggie zhu <107164272+magizee@users.noreply.github.com>
2024-10-01 16:08:31 -0400

    motortest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/OpModes/Tests/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/OpModes/Tests/MotorTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Bot.OpModes.Tests;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 /*
 practice motor test without using codebase
@@ -11,41 +12,48 @@ practice motor test without using codebase
 public class MotorTest extends LinearOpMode {
     DcMotor motor;
     private int targetPosition;
+    private double velocity = 1;
+    private final int INCREMENT = 100;
+    private final double V_INCREMENT = 0.0000001;
+
     @Override
     public void runOpMode () {
         motor = hardwareMap.get(DcMotor.class, "motor");
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         telemetry.addData("Status", "Initialized");
-        telemetry.update();
 
         waitForStart();
-        int updateCounter = 0;
-        int updateThreshold = 6;  // Change targetPosition every 6 iterations (slows it to 10 times per second)
 
         while (opModeIsActive()) {
-            // Increment counter each loop iteration
-            updateCounter++;
-
-            // Only update the target position every updateThreshold iterations
-            if (updateCounter >= updateThreshold) {
-                if (gamepad1.left_stick_y < -0.04) {
-                    targetPosition += 1;
-                }
-                if (gamepad1.left_stick_y > 0.04) {
-                    targetPosition -= 1;
-                }
+            if (gamepad1.left_stick_y < -0.4) {
+                targetPosition += 1;
+            }
+            if (gamepad1.left_stick_y > 0.4) {
+                targetPosition -= 1;
             }
 
-            updateCounter = 0;
-
-            telemetry.addData("TargetPosition", targetPosition);
-            telemetry.update();
+            if (gamepad1.right_stick_y < -0.4) {
+                velocity += V_INCREMENT;
+            }
+            if (gamepad1.right_stick_y > 0.4) {
+                velocity -= V_INCREMENT;
+            }
 
             if (gamepad1.left_bumper) {
-                motor.setTargetPosition(targetPosition);
+                motor.setTargetPosition(targetPosition / INCREMENT);
                 motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                motor.setPower(1);
+                motor.setPower(Range.clip(velocity, 0, 1));
             }
-        }
 
+            if (gamepad1.right_bumper) {
+                motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                motor.setPower(Range.clip(velocity, 0, 1));
+            }
+            telemetry.addData("TargetPosition", targetPosition / INCREMENT);
+            telemetry.addData("velocity", velocity);
+
+            telemetry.update();
+        }
     }
 }
