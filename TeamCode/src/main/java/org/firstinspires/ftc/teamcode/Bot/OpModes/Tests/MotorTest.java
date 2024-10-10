8308826b82d7e93b7b57ef9169114755dc00caa2
imsmelkov <imsmelkov@gmail.com>
2024-10-10 16:10:11 -0400

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/OpModes/Tests/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot/OpModes/Tests/MotorTest.java
@@ -43,6 +43,7 @@ public class MotorTest extends LinearOpMode {
             }
             else{
                 counter++;
+
             }
             if(gamepad1.a){
                 velocity*=-1;
